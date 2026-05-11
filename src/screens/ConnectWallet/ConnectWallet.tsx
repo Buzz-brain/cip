2dea55f3408380b33cbc207524c08a2cca31f706
Buzz-brain <chinomsochristian03@gmail.com>
2026-05-12 00:28:26 +0100

    Fixing the WalletConnect Modal

--- src/screens/ConnectWallet/ConnectWallet.tsx
+++ src/screens/ConnectWallet/ConnectWallet.tsx
@@ -53,19 +53,6 @@ const wallets = [
     category: "Multi-chain",
     icon: trustWallet,
   },
-  // {
-  //   id: "coinbase",
-  //   name: "Coinbase Wallet",
-  //   description: "Exchange Connected",
-  //   icon: coinbaseWallet,
-  // },
-  // {
-  //   id: "ledger",
-  //   name: "Ledger",
-  //   description: "Cold Storage",
-  //   category: "Hardware",
-  //   icon: ledger,
-  // },
 ];
 
 /**
